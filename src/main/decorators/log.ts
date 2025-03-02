76079d179229aa9613e44aa990df63307f750c5e
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-02 09:11:08 -0300

    feat: add LogControllerDecorator for error logging in controller responses

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,27 @@
+import { LogErrorRepository } from '../../data/protocols/log-error-repository';
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from '../../presentation/protocols';
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller;
+  private readonly logErrorRepository: LogErrorRepository;
+
+  constructor(controller: Controller, logErrorRepository: LogErrorRepository) {
+    this.controller = controller;
+    this.logErrorRepository = logErrorRepository;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest);
+
+    const { statusCode, body } = httpResponse;
+    const isServerError = statusCode >= 500 && statusCode <= 599;
+
+    if (isServerError) await this.logErrorRepository.logError(body.stack);
+
+    return httpResponse;
+  }
+}
