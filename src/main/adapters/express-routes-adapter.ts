83ecc621e2148492fdb1b99214806b6e9dbe53b6
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-02 10:37:22 -0300

    feat: enhance error handling in express route adapter for improved response consistency

--- src/main/adapters/express-routes-adapter.ts
+++ src/main/adapters/express-routes-adapter.ts
@@ -5,7 +5,15 @@ export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = { body: req.body };
     const httpResponse = await controller.handle(httpRequest);
+    const { statusCode, body } = httpResponse;
+    const error = { error: body.message };
 
-    res.status(httpResponse.statusCode).json(httpResponse.body);
+    if (statusCode !== 200) {
+      res.status(statusCode).json(error);
+
+      return;
+    }
+
+    return res.status(statusCode).json(body);
   };
 };
