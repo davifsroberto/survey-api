6ff25dd63156a05e345b3d274f3b8affd5734b4a
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-02 14:20:11 -0300

    feat: enhance LoginController to validate email format and handle missing parameters

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,13 +1,35 @@
 import { badRequest } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+import { EmailValidator } from '../signup/signup-protocols';
+import { MissingParamError } from '../../erros';
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+    const { body } = httpRequest;
+    const { email } = body;
+    const requiredFields = ['email', 'password'];
 
-    if (!email) return badRequest(new Error('Missing param: email'));
+    for (const field of requiredFields) {
+      if (!body[field]) {
+        return new Promise((resolve) => {
+          resolve(badRequest(new MissingParamError(field)));
+        });
+      }
+    }
 
-    if (!password) return badRequest(new Error('Missing param: password'));
+    const isValidEmail = this.emailValidator.isValid(email);
+
+    if (!isValidEmail) {
+      return new Promise((resolve) =>
+        resolve(badRequest(new Error('Invalid param: email'))),
+      );
+    }
 
     return new Promise((resolve) => resolve({ statusCode: 200, body: {} }));
   }
