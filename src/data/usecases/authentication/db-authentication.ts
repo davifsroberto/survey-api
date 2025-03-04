79bb1bdee64606bb4bfd5ba3f088d98ae3091510
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-04 12:18:21 -0300

    feat: add HashComparer interface and integrate it into DbAuthentication for password comparison

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,17 +2,27 @@ import {
   Authentication,
   AuthenticationModel,
 } from '../../../domain/usecases/authentication';
+import { HashComparer } from '../../protocols/criptography/hash-compare';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
+  private readonly hashComparer: HashComparer;
 
-  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor(
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+    this.hashComparer = hashComparer;
   }
 
   async auth(authentication: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email);
+    const { email, password } = authentication;
+
+    const account = await this.loadAccountByEmailRepository.load(email);
+
+    account && (await this.hashComparer.compare(password, account.password));
 
     return null as unknown as string;
   }
