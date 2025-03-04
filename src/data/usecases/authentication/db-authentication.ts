75dd0714212f753038319ca9e06f1696b45f07a0
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-04 13:08:26 -0300

    refactor: improve authentication flow by validating account before generating access token

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -5,6 +5,7 @@ import {
 import { HashComparer } from '../../protocols/criptography/hash-compare';
 import { TokenGenerator } from '../../protocols/criptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
+import { AccountModel } from '../add-account/db-add-account-protocols';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
@@ -22,20 +23,28 @@ export class DbAuthentication implements Authentication {
   }
 
   async auth(authentication: AuthenticationModel): Promise<string> {
+    const accountValidated = await this.validateAccount(authentication);
+
+    if (!accountValidated) return null as unknown as string;
+
+    return this.generateAccessToken(accountValidated.id);
+  }
+
+  private async validateAccount(
+    authentication: AuthenticationModel,
+  ): Promise<AccountModel | null> {
     const { email: authEmail, password: authPass } = authentication;
+
     const account = await this.loadAccountByEmailRepository.load(authEmail);
+    if (!account) return null;
 
-    if (!account) return null as unknown as string;
+    const isValid = await this.hashComparer.compare(authPass, account.password);
+    if (!isValid) return null;
 
-    const hashComparer = await this.hashComparer.compare(
-      authPass,
-      account.password,
-    );
+    return account;
+  }
 
-    if (!hashComparer) return null as unknown as string;
-
-    const accessToken = await this.tokenGenerator.generate(account.id);
-
-    return accessToken;
+  private async generateAccessToken(accountId: string): Promise<string> {
+    return this.tokenGenerator.generate(accountId);
   }
 }
