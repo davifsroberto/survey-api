ee857e47464670b65aace526efd024beb3440ceb
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-04 11:53:17 -0300

    refactor: reorganize repository protocols and update imports

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../usecases/add-account/db-add-account-protocols';
+import { AccountModel } from '../../usecases/add-account/db-add-account-protocols';
 
 export interface LoadAccountByEmailRepository {
   // eslint-disable-next-line no-unused-vars
