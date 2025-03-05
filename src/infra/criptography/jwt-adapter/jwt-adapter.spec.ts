2c0331782e2682c8bc4b9f4c1272bd2ed787a029
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-05 16:13:13 -0300

    test: add unit test for successful token generation in JwtAdapter

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 import { JwtAdapter } from './jwt-adapter';
+
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return new Promise((resolve) => resolve('any_token'));
+  },
+}));
+
 describe('JWT Adapter', () => {
   it('Should call a sing with correct values', async () => {
     const sut = new JwtAdapter('secret');
@@ -9,5 +16,12 @@ describe('JWT Adapter', () => {
     await sut.encrypt('any_id');
 
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  it('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret');
+    const accessToken = await sut.encrypt('any_id');
+
+    expect(accessToken).toBe('any_token');
   });
 });
