30c037202270af5962a55c0998560d20cc97e2ad
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-05 15:12:18 -0300

    test: add failure case for compare method in BcryptAdapter tests

--- src/infra/criptography/bcrypt-adatper.spec.ts
+++ src/infra/criptography/bcrypt-adatper.spec.ts
@@ -59,4 +59,12 @@ describe('Bcrypt Adapter', () => {
 
     expect(isValid).toBe(true);
   });
+
+  it('Shold return false when compare fails', async () => {
+    const sut = makeSut();
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => false);
+    const isValid = await sut.compare('any_value', 'any_hash');
+
+    expect(isValid).toBe(false);
+  });
 });
