8e8e5abb588a24a5deb5d584d61a95aad8c2efb4
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-06 19:13:17 -0300

    refactor: rename singnup routes to login

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(String(process.env.MONGO_URL));
   });
@@ -18,15 +18,17 @@ describe('Signup Routes', () => {
     await MongoHelper.disconnect();
   });
 
-  it('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Davi Roberto',
-        email: 'davifsroberto@outlook.com',
-        password: '12345',
-        passwordConfirmation: '12345',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    it('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Davi Roberto',
+          email: 'davifsroberto@outlook.com',
+          password: '12345',
+          passwordConfirmation: '12345',
+        })
+        .expect(200);
+    });
   });
 });
