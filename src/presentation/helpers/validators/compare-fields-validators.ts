d2f5e5be5ec5c30e34e1cdd51f5c6be38bbcba24
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-06 19:01:07 -0300

    refactor: simplify constructor parameters across multiple classes

--- src/presentation/helpers/validators/compare-fields-validators.ts
+++ src/presentation/helpers/validators/compare-fields-validators.ts
@@ -2,13 +2,10 @@ import { InvalidParamError } from '../../erros';
 import { Validation } from '../../protocols/validation';
 
 export class CompareFieldsValidation implements Validation {
-  private readonly fieldName: string;
-  private readonly fieldNameToCompare: string;
-
-  constructor(fieldName: string, fieldNameToCompare: string) {
-    this.fieldName = fieldName;
-    this.fieldNameToCompare = fieldNameToCompare;
-  }
+  constructor(
+    private readonly fieldName: string,
+    private readonly fieldNameToCompare: string,
+  ) {}
 
   validate<T>(input: T): Error {
     if (input[this.fieldName] !== input[this.fieldNameToCompare]) {
