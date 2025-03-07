05546c28142a51a8cda7e0b82b37e0ba43da9cdf
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-07 11:34:42 -0300

    test: add error handling test for Authentication in SignUpController

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -150,4 +150,15 @@ describe('SignUp Controller', () => {
       password: 'any_password',
     });
   });
+
+  it('Should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest
+      .spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(new Promise((_, reject) => reject(new Error())));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse.statusCode).toBe(500);
+  });
 });
