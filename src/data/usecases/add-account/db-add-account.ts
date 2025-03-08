390f399e264cd6d0bd7af22f7d0f651378d3da26
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-08 11:58:11 -0300

    feat: update DbAddAccount to return null if account already exists

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -14,13 +14,19 @@ export class DbAddAccount {
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
-
-    const hashedPassword = await this.hasher.hash(accountData.password);
-    const account = await this.addAccountRepository.add(
-      Object.assign({}, accountData, { password: hashedPassword }),
+    const account = await this.loadAccountByEmailRepository.loadByEmail(
+      accountData.email,
     );
 
-    return new Promise((resolve) => resolve(account));
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password);
+      const newAccount = await this.addAccountRepository.add(
+        Object.assign({}, accountData, { password: hashedPassword }),
+      );
+
+      return newAccount;
+    }
+
+    return null;
   }
 }
