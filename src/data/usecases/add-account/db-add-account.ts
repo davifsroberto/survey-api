25239d042b249e8c659b8fd7442435d0c29875c9
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-08 11:31:07 -0300

    feat: add LoadAccountByEmailRepository to DbAddAccount for email existence check

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,15 +3,19 @@ import {
   AddAccountModel,
   AddAccountRepository,
   Hasher,
+  LoadAccountByEmailRepository,
 } from './db-add-account-protocols';
 
 export class DbAddAccount {
   constructor(
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
+
     const hashedPassword = await this.hasher.hash(accountData.password);
     const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword }),
