741e3724ab464a8323683fce7012069337180b24
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-08 13:08:49 -0300

    feat: add email validation functionality with required and comparison checks

--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -1,6 +1,6 @@
-import { InvalidParamError } from '../../erros';
-import { EmailValidator } from '../../protocols/email-validator';
-import { Validation } from '../../protocols/validation';
+import { InvalidParamError } from '../../presentation/erros';
+import { EmailValidator } from '../potocols/email-validator';
+import { Validation } from '../../presentation/protocols';
 
 export class EmailValidation implements Validation {
   constructor(
